8cf839d2e1a97c63ececd8327ae80979f9f1b732
Elliot Duran Macedo <91506226+ElliotDM@users.noreply.github.com>
2022-06-01 15:09:37 -0500

    Dinero y cambio
    Se añadió la salida del dinero introducido y el cambio devuelto a los displays de 7 segmentos

--- Botones.vhd
+++ Botones.vhd
@@ -1,3 +1,10 @@
+-- Precio de los productos
+--		Coca-Cola $15
+--		Takis     $12
+--		Leche     $13
+--		Sauris    $10
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
@@ -6,15 +13,27 @@ use ieee.std_logic_unsigned.all;
 entity Botones is
 	port (
 		clk      : in  std_logic;
+		-- Botones
 		btn1     : in  std_logic;
 		btn5     : in  std_logic;
+		-- Teclado
 		col      : in 	std_logic_vector(3 downto 0);
 		ren      : out std_logic_vector(3 downto 0);
-		display1 : out std_logic_vector(6 downto 0)
+		-- Motores
+		MOT1     : out std_logic_vector(3 downto 0);
+		MOT2     : out std_logic_vector(3 downto 0);
+		MOT3     : out std_logic_vector(3 downto 0);
+		MOT4     : out std_logic_vector(3 downto 0);
+		-- Displays
+		display1 : out std_logic_vector(6 downto 0);
+		display2 : out std_logic_vector(6 downto 0);
+		display3 : out std_logic_vector(6 downto 0)
 	);
 end Botones;
 
 architecture rtl of Botones is
+
+	constant delayms : integer := (50_000_000/10000)-1;
 
 	component TecMatrix is
 		generic (freq_clk: integer := 50_000_00);
@@ -36,7 +55,7 @@ architecture rtl of Botones is
 		generic(
     		clk_freq    : integer := 50_000_000;  --system clock frequency in Hz
     		stable_time : integer := 10);         --time button must remain stable in ms
-		PORT(
+		port(
 			clk     : in  std_logic;  --input clock
 			reset_n : in  std_logic;  --asynchronous active low reset
 			button  : in  std_logic;  --input signal to be debounced
@@ -45,41 +64,58 @@ architecture rtl of Botones is
 	end component;
 	
 	signal res1, res5 : std_logic := '0';
-	signal cuenta1, cuenta5 : integer range 0 to 15 := 15;
+	signal cuenta1, cuenta5 : integer := 0;
 	signal moneda1 : std_logic;
 	signal moneda5 : std_logic;
 	
-	signal precio : integer range 0 to 15 := 0;
-	signal total  : integer range 0 to 15 := 0;
+	component Pasos is
+		port ( 
+			clk  : in  std_logic;
+			rst  : in  std_logic;
+			dir  : in  std_logic;
+			MOT  : out std_logic_vector(3 downto 0)
+		);
+	end component;
 	
-	constant delayms : integer := (50_000_00/10000)-1;
+	signal rstM1, rstM2, rstM3, rstM4 : std_logic := '1';
+	
+	signal precio, unidades, decenas : integer range 0 to 99 := 99;
+	signal cambio : integer range 0 to 10 := 0;
+	signal sele : std_logic := '0';
 
 begin
 
 	T0 : TecMatrix generic map(50_000_000) 
 		port map(clk, col, ren, btn_pres, ind);
-	B1 : debounce port map(clk, '1', btn1, res1);
-	B5 : debounce port map(clk, '1', btn5, res5);
 	
+	B1 : debounce generic map(50_000_000, 10) port map(clk, '1', btn1, res1);
+	B5 : debounce generic map(50_000_000, 10) port map(clk, '1', btn5, res5);
+	
+	M1 : Pasos port map(clk, rstM1, '1', MOT1);
+	M2 : Pasos port map(clk, rstM2, '0', MOT2);
+	M3 : Pasos port map(clk, rstM3, '1', MOT3);
+	M4 : Pasos port map(clk, rstM4, '1', MOT4);
+	
+	-- Seleccion del producto y asignacion del precio --
 	process(clk)
 		variable conta : integer := 0;
 	begin
 		if rising_edge(clk) then
 			if ind = '1' and btn_pres = x"1" then 
 				code   <= "001";
-				precio <= 15;	--f
+				precio <= 15;
 				init   <= '1';
 			elsif ind = '1' and btn_pres = x"2" then 
 				code   <= "010";
-				precio <= 12;	--c
+				precio <= 12;
 				init   <= '1';
 			elsif ind = '1' and btn_pres = x"3" then 
 				code   <= "011";
-				precio <= 13;	--d
+				precio <= 13;
 				init   <= '1';
 			elsif ind = '1' and btn_pres = x"4" then 
 				code   <= "100";
-				precio <= 10;	--a
+				precio <= 10;
 				init   <= '1';
 			else
 				code <= code;
@@ -93,50 +129,103 @@ begin
 		end if;
 	end process;
 	
-	process(init, res1) is -- Boton moneda de $1
+	-- Sumador de $1 --
+	process(init, res1)	
 	begin
 		if init = '1' then
 			cuenta1 <= 0;
 		elsif rising_edge(res1) then
 			if cuenta1 = precio then
-				cuenta1 <= cuenta1 + 1;
-			else
 				cuenta1 <= 0;
+			else
+				cuenta1 <= cuenta1 + 1;
 			end if;
 		end if;
 	end process;
 	
-	process(res5) is -- Boton moneda de $5
+	-- Sumador de $5 --
+	process(init, res5)
 	begin
 		if init = '1' then
 			cuenta5 <= 0;
 		elsif rising_edge(res5) then
-			if cuenta5 = precio then
-				cuenta5 <= cuenta5 + 5;
-			else
+			if cuenta5 >= precio then
 				cuenta5 <= 0;
+			else
+				cuenta5 <= cuenta5 + 5;
 			end if;
 		end if;
 	end process;
 	
-	total <= cuenta1 + cuenta5;
+	-- Asignacion de dinero y cambio --
+	unidades <= cuenta1 + cuenta5;
+	decenas  <= cuenta1 + cuenta5;
+	cambio   <= unidades - precio;
 	
-	with total select
-		display1 <= "1000000" when 0,
-			"1111001" when 1,
-			"0100100" when 2,
-			"0110000" when 3,
-			"0011001" when 4,
-			"0010010" when 5,
-			"0000010" when 6,
-			"1111000" when 7,
-			"0000000" when 8,
-			"0010000" when 9,
-			"0001000" when 10,--a
-			"0000011" when 11,--b 
-			"1000110" when 12,--c
-			"0100001" when 13,--d
-			"0000110" when 14,--e
-			"0001110" when 15;--f
+	-- Activacion de motores --
+	process(clk)	
+	begin
+		if clk'event and clk = '1' then
+			if    code = "001" and unidades >= precio then 
+				rstM1 <= '0';
+				sele  <= '1';
+			elsif code = "010" and unidades >= precio then 
+				rstM2 <= '0';
+				sele  <= '1';
+			elsif code = "011" and unidades >= precio then 
+				rstM3 <= '0';
+				sele  <= '1';
+			elsif code = "100" and unidades >= precio then 
+				rstM4 <= '0';
+				sele  <= '1';
+			end if;
+		end if;
+	end process;
+	
+	-- Displays dinero colocado --
+	with unidades select display1 <=	
+		"1111001" when 1,
+		"0100100" when 2,
+		"0110000" when 3,
+		"0011001" when 4,
+		"0010010" when 5,
+		"0000010" when 6,
+		"1111000" when 7,
+		"0000000" when 8,
+		"0010000" when 9,
+		"1000000" when 10,
+		"1111001" when 11,
+		"0100100" when 12,
+		"0110000" when 13,
+		"0011001" when 14,
+		"0010010" when 15,
+		"1111111" when others;
+	
+	with decenas select display2 <= 
+		"1111001" when 10,
+		"1111001" when 11,
+		"1111001" when 12,
+		"1111001" when 13,
+		"1111001" when 14,
+		"1111001" when 15,
+		"1111111" when others;
+	
+	-- Display cambio --
+	process(sele)
+	begin
+		if sele = '1' then
+			case cambio is
+				when 1 => display3 <= "1111001";
+				when 2 => display3 <= "0100100";
+				when 3 => display3 <= "0110000";
+				when 4 => display3 <= "0011001";
+				when others => display3 <= "1111111";
+			end case;
+		else
+			case cambio is
+				when others => display3 <= "1111111";
+			end case;
+		end if;
+	end process;
 
 end rtl;
